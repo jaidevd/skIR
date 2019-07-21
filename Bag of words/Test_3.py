b718742c932dd1a730640b832078d222778f93b0
Ashishjaiswal403 <52210524+Ashishjaiswal403@users.noreply.github.com>
2019-07-21 22:01:59 +0530

    Add files via upload

---
+++ Bag of words/Test_3.py
@@ -0,0 +1,22 @@
+import time
+import json
+import tornado
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+vectorizer=TfidfVectorizer()
+text=open('newfile.txt')
+corpus = text.readlines()
+text.close()
+X = vectorizer.fit_transform(corpus)
+Y=X.toarray()
+
+def transform(sample1):
+    X = vectorizer.transform([sample1])
+    p=cosine_similarity(X,Y).ravel().argmax()
+    return corpus[p]
+
+def Query(handler):
+    query= handler.get_argument("query")
+    return transform(query)
