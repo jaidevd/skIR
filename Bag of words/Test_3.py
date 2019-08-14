30926292d5ae30c9eb397ae65d572d624d582836
VasundharaAgarwal <50448214+VasundharaAgarwal@users.noreply.github.com>
2019-08-14 10:16:31 +0530

    Add files via upload

--- Bag of words/Test_3.py
+++ Bag of words/Test_3.py
@@ -19,4 +19,6 @@ def transform(sample1):
 
 def Query(handler):
     query= handler.get_argument("query")
+    #handler.set_header('Content-Disposition'",value="k")
+    
     return transform(query)
