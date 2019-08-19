3cb98072a64a946d60a0c07154cb8385d1367cce
VasundharaAgarwal <50448214+VasundharaAgarwal@users.noreply.github.com>
2019-08-19 16:09:40 +0530

    Add files via upload

--- Bag of words/Test_3.py
+++ Bag of words/Test_3.py
@@ -1,22 +1,45 @@
-import time
-import json
-import tornado
+
+
+import tornado  
+
+import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-
 vectorizer=TfidfVectorizer()
-text=open('newfile.txt')
-corpus = text.readlines()
-text.close()
+file_list = [f for f in os.listdir('.') if os.path.isfile(os.path.join('.', f)) and f.endswith("csv")]
+
+fcmap={}
+columnf= open("columns","w+")
+for f in file_list:
+    csvf = open(f)
+    columnl=csvf.__next__().split(",")
+    fcmap[f]=columnl
+    for word in columnl:
+        columnf.write(word+"\n")
+    csvf.close()
+
+columnf.close()
+columnf=open("columns")
+corpus = columnf.readlines()
 X = vectorizer.fit_transform(corpus)
-Y=X.toarray()
+Y = X.toarray()
+
+
+
 
 def transform(sample1):
     X = vectorizer.transform([sample1])
     p=cosine_similarity(X,Y).ravel().argmax()
-    return corpus[p]
-
+    word=corpus[p][:-1]
+    filename=""
+    for f in fcmap:
+        columnl=fcmap[f]
+        for column in columnl:
+            if(word==column):
+                filename=f
+    return ("File : "+filename+" Column : "+corpus[p])
 def Query(handler):
     query= handler.get_argument("query")
+    #handler.set_header('Content-Disposition'",value="k")
     return transform(query)
